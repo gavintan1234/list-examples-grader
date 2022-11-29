0dfb37cb225e71a2740aaf84ba29ebffcd0bb643
gavintan1234 <gttan@ucsd.edu>
2022-11-28 20:35:03 -0800

    Created grading script and tests

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,10 +1,45 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
+
+class ContainsA implements StringChecker {
+  public boolean checkString(String s) {
+    return s.contains("a");
+  }
+}
 
 public class TestListExamples {
   @Test(timeout = 100)
-  public void testTimeout() {
-    while(true) {}
+  public void testFilter(){
+    List<String> input = Arrays.asList(new String[]{"average", "power", "car", "window"});
+    List<String> expected = Arrays.asList(new String[]{"average", "car"});
+
+    assertArrayEquals(expected.toArray(), (ListExamples.filter(input, new ContainsA())).toArray());
   }
-  // Write your grading tests here!
+
+  @Test(timeout = 100)
+  public void testFilterDuplicates(){
+    List<String> input = Arrays.asList(new String[]{"average", "average", "average", "window"});
+    List<String> expected = Arrays.asList(new String[]{"average", "average", "average"});
+
+    assertArrayEquals(expected.toArray(), (ListExamples.filter(input, new ContainsA())).toArray());
+  }
+
+  @Test(timeout = 100)
+  public void testMerge(){
+    List<String> input1 = Arrays.asList(new String[]{"a", "c", "p", "w"});
+    List<String> input2 = Arrays.asList(new String[]{"b", "e", "f", "t"});
+    List<String> expected = Arrays.asList(new String[]{"a", "b", "c", "e", "f", "p", "t", "w"});
+
+    assertArrayEquals(expected.toArray(), (ListExamples.merge(input1, input2)).toArray());
+  }
+
+  @Test(timeout = 100)
+  public void testMergeDuplicates(){
+    List<String> input1 = Arrays.asList(new String[]{"a", "b", "b", "e"});
+    List<String> input2 = Arrays.asList(new String[]{"b", "e", "e", "t"});
+    List<String> expected = Arrays.asList(new String[]{"a", "b", "b", "b", "e", "e", "e", "t"});
+
+    assertArrayEquals(expected.toArray(), (ListExamples.merge(input1, input2)).toArray());
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,9 +1,33 @@
-# Create your grading script here
-
+CP=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+cp TestListExamples.java student-submission/
+cd student-submission/
 
+if [[ !(-e ListExamples.java) ]]; then
+    echo "ListExamples.java not found!"
+    exit 1
+fi
 
+javac -cp $CP ListExamples.java
+if [[ $? -ne 0 ]]; then
+    echo "ListExamples.java failed to compile!"
+    exit 1
+fi
 
+javac -cp $CP TestListExamples.java
+if [[ $? -ne 0 ]]; then
+    echo "TestListExamples.java failed to compile!"
+    exit 1
+fi
 
+java -cp $CP org.junit.runner.JUnitCore TestListExamples|head -n 2|tail -n 1 > TestOutput.txt
+
+NUMTESTS=$(grep -o '.' TestOutput.txt|wc -l)
+NUMFAILED=$(grep -o 'E' TestOutput.txt|wc -l)
+NUMPASSED=$(($NUMTESTS - $NUMFAILED))
+
+echo "Number of tests passed: $NUMPASSED"
+echo "Number of tests failed: $NUMFAILED"
+echo "Final score: $NUMPASSED/$NUMTESTS"
